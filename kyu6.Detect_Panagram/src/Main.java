1686b47145783c4580a8ffc2c11a1b7fb49a90d7
ritwinkle <rikale.aw@gmail.com>
2019-11-04 21:54:24 +0300

    Lazy fast kata

---
+++ kyu6.Detect_Panagram/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println(check("The quick brown fox jumps over the lazy dog."));
+    }
+
+    public static boolean check(String sentence){
+        return sentence.chars().map(Character::toLowerCase).filter(Character::isAlphabetic).distinct().count() == 26;
+    }
+}
